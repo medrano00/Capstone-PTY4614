349aa3636f764928680269f769128756a5550c64
Fabián Espinoza M. <faboespinozam@icloud.com>
2024-10-03 00:41:33 -0300

    Proyecto Base - ~60% de avance
    TO-DO:
        - Arreglar tema de Apoderados y Niños
        - Asistencia de Niños
        - Subida de Archivos
        - Subida a Hosting y posible compra de dominio

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Apoderado, Niño
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_parvularia', 'is_apoderado')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_parvularia', 'is_apoderado')
+
+@admin.register(Apoderado)
+class ApoderadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'rut', 'telefono', 'email', 'direccion', 'user__is_parvularia', 'user__is_apoderado')
+    search_fields = ('nombre', 'apellido', 'rut', 'telefono', 'email', 'direccion')
+    list_filter = ('user__is_parvularia', 'user__is_apoderado')
+
+@admin.register(Niño)
+class NiñoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'fecha_nacimiento', 'edad', 'telefono', 'email', 'direccion')
+    search_fields = ('nombre', 'apellido', 'fecha_nacimiento', 'edad', 'telefono', 'email', 'direccion')
+    list_filter = ('apoderado__user__is_parvularia', 'apoderado__user__is_apoderado')
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import *
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name='home'),
-]
+    path('', views.index, name= 'index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('parvularia/', views.parvularia, name='parvularia'),
+    path('apoderado/', views.apoderado, name='apoderado'),
+    path('apoderado/perfilNiño/', views.perfilNiño, name='perfilNiño'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
